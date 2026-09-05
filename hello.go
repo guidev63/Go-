35f4cd02a1e4ace70881853c7e860c9bd812a95e
Guilherme Augusto <guilhermedev23@gmail.com>
2026-09-04 22:26:07 -0300

    aula go concluída

---
+++ hello.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	exibeIntroducao()
+	exibeMenu()
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do Programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não Conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func exibeIntroducao() {
+	nome := "Douglas"
+	versao := 1.1
+
+	fmt.Println("Olá, Sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func leComando() int {
+	var comandoLindo int
+
+	fmt.Scan(&comandoLindo)
+
+	fmt.Println("O endereço da minha variável comando é", &comandoLindo)
+	fmt.Println("O comando escolhido foi", comandoLindo)
+
+	return comandoLindo
+}
+
+func iniciaMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+	fmt.Println(resp)
+}
