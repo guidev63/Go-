c777b2ef27a57fa2adc9f4bbe0e8ba5eea0cf201
Guilherme Augusto <guilhermedev23@gmail.com>
2026-09-16 22:08:25 -0300

    Minha primeira struct

--- hello.go
+++ hello.go
@@ -3,8 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,6 +16,7 @@ const monitoramentos = 3
 const delay = 5
 
 func main() {
+	registraLog("site-falso", false)
 
 	exibeIntroducao()
 	leSitesDoArquivo()
@@ -26,6 +31,7 @@ func main() {
 			iniciaMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do Programa")
 			os.Exit(0)
@@ -96,8 +102,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == http.StatusOK {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -109,6 +117,39 @@ func leSitesDoArquivo() []string {
 	if err != nil {
 		fmt.Println("Ocorreu um erro:", err)
 	}
-	bufio.NewReader(arquivo)
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
 	return sites
+}
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.Txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + " " + site + "- online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(arquivo))
 }
